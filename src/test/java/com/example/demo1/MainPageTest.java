f55b507def6828b7fe218b388e02503210070a7d
@sahanl <sahanl@hsenidmobile.com>
2021-10-13 17:16:06 +0530

    commit1

--- src/test/java/com/example/demo1/MainPageTest.java
+++ src/test/java/com/example/demo1/MainPageTest.java
@@ -1,31 +1,52 @@
 package com.example.demo1;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Configuration.browser;
-import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 
 public class MainPageTest {
 
     //private OpenTwitterPage page = new OpenTwitterPage();
 
-    @BeforeClass
-    void beforeAll(){
-//        Configuration.remote = "http://localhost:4444/wd/hub";
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("enableVNC", true);
-//        capabilities.setCapability("browserName", "chrome");
-//        Configuration.browserCapabilities = capabilities;
-        browser = "firefox";
+    @BeforeAll
+    static void setUp(){
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        Configuration.browser="firefox";
+        Configuration.browserCapabilities = capabilities;
+
+//        if (browserName.equals("chrome")){
+//            WebDriverManager.chromedriver().setup();
+//            capabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+//        }
+//        else if (browserName.equals("firefox")){
+//            WebDriverManager.firefoxdriver().setup();
+//            capabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
+//        }
+//        else if (browserName.equals("edge")){
+//            //WebDriverManager.edgedriver().setup();
+//            capabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.EDGE);
+//        }
+
+//        try {
+//            driver = new RemoteWebDriver(new URL(url), capabilities);
+//            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+
     }
 
     @Test
-    void openPage() throws InterruptedException {
+    public void openPage() throws InterruptedException {
 
-        com.codeborne.selenide.Selenide.open("https://twitter.com/i/flow/login");
-        System.out.println("hello");
+        open("https://twitter.com/i/flow/login");
+
         //page.userNameInput();
         Thread.sleep(6000);
         //page.passwordInput();
@@ -36,11 +57,7 @@ public class MainPageTest {
 
         //page.openTweet();
 
-       //Thread.sleep(6000);
+        //Thread.sleep(6000);
 
-    }
-    @AfterClass
-    public static void logout() {
-        closeWebDriver();
     }
 }
